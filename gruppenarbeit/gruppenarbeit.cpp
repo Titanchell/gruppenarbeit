d9430cd213cd3422620c5c1ccaf8b3f50ce02635
Titanchell <jonas.h.bentz@gmail.com>
2025-05-16 12:33:52 +0200

    Kommentare

--- gruppenarbeit/gruppenarbeit.cpp
+++ gruppenarbeit/gruppenarbeit.cpp
@@ -12,6 +12,7 @@ struct Mitarbeiter {
     float gehalt;
 };
 
+// Liest bestehende Datensätze aus der Datei und speichert sie im Vektor
 void datenLaden(vector<Mitarbeiter>& daten) {
     ifstream file("../TextFile1.txt", ios::in);
     if (!file) return;
@@ -23,6 +24,7 @@ void datenLaden(vector<Mitarbeiter>& daten) {
     file.close();
 }
 
+// Schreibt alle Datensätze aus dem Vektor in die Datei (überschreibt alte Datei)
 void datenSpeichern(const vector<Mitarbeiter>& daten) {
     ofstream file("../TextFile1.txt");
     for (const auto& s : daten) {
@@ -34,10 +36,11 @@ void datenSpeichern(const vector<Mitarbeiter>& daten) {
 
 int main() {
     vector<Mitarbeiter> daten;
-    datenLaden(daten);
-    Mitarbeiter m;
+    datenLaden(daten);  // Bestehende Daten beim Start laden
+
     int wahl;
     do {
+        // Menüauswahl anzeigen
         cout << "\n--- Mitarbeiterprogramm ---\n";
         cout << "1. Datensatz hinzufuegen\n";
         cout << "2. Alle Datensaetze anzeigen\n";
@@ -45,27 +48,24 @@ int main() {
         cin >> wahl;
 
         if (wahl == 1) {
+            // Neuen Datensatz erfassen und speichern
             Mitarbeiter s;
-            cout << "Personalnummer: "; 
-            cin >> s.personalnummer;
-            cout << "Vorname: "; 
-            cin >> s.vorname;
-            cout << "Nachname: "; 
-            cin >> s.nachname;
-            cout << "Geburtsdatum (TT.MM.JJJJ): "; 
-            cin >> s.geburtsdatum;
-            cout << "Gehalt: "; 
-            cin >> s.gehalt;
+            cout << "Personalnummer: "; cin >> s.personalnummer;
+            cout << "Vorname: "; cin >> s.vorname;
+            cout << "Nachname: "; cin >> s.nachname;
+            cout << "Geburtsdatum (TT.MM.JJJJ): "; cin >> s.geburtsdatum;
+            cout << "Gehalt: "; cin >> s.gehalt;
             daten.push_back(s);
             datenSpeichern(daten);
         }
         else if (wahl == 2) {
-            for (const auto& m : daten) { 
+            // Alle gespeicherten Datensätze anzeigen
+            for (const auto& m : daten) {
                 cout << m.personalnummer << ": " << m.vorname << " " << m.nachname
                     << ", " << m.geburtsdatum << ", Gehalt: " << m.gehalt << " EUR\n";
             }
         }
 
-    } while (wahl != 0);
+    } while (wahl != 0);  // Programm läuft bis zur Eingabe von 0
     return 0;
 }
